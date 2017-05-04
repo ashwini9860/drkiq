8299d59ea29782f75fee14c8690bcfb1e897cf94
root <root@ip-10-0-205-31.ec2.internal>
2017-05-04 13:15:44 +0000

    sidekiq.yml chnages

--- app/jobs/counter_job.rb
+++ app/jobs/counter_job.rb
@@ -2,7 +2,10 @@ class CounterJob < ActiveJob::Base
   queue_as :default
   def perform(*args)
    # DateTime.now #21+21 # Do something later
-   
+   (1...10).each{|i|
+    sleep(3)
+    puts "-=-=-=-=#{i}-=-=-=-=wq"
+   }
    DateTime.now
   end
 end

45a528ee0b96f2f6eef2f6c1abe435046789d44d
ashwini9860 <auchoudhari@gmail.com>
2017-05-04 09:26:06 +0000

    changes for user

--- config/routes.rb
+++ config/routes.rb
@@ -1,11 +1,13 @@
 Rails.application.routes.draw do
+  root "pages#show", page: "home"  
+devise_for :users
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
   # You can have the root of your site routed with "root"
   # root 'welcome#index'
    get "/pages/:page" => "pages#show"
-   root "pages#show", page: "home"
+#   root "pages#show", page: "home"
   # Example of regular route:
   #   get 'products/:id' => 'catalog#view'
 
